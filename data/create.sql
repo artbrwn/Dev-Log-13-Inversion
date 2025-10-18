e707788881066e53c313e93387f014ea5b30a532
Arturo <md.arturo@gmail.com>
2025-10-18 18:36:47 +0200

    Creación del archivo create.sql con la estructura de la tabla en la base de de datos

---
+++ data/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "transaction" (
+	"id"	INTEGER NOT NULL,
+	"date"	TEXT NOT NULL,
+	"time"	TEXT NOT NULL,
+	"currency_from"	TEXT NOT NULL,
+	"amount_from"	REAL NOT NULL,
+	"currency_to"	TEXT NOT NULL,
+	"amount_to"	REAL NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
