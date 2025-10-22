f9edf94691f621055f578abdf75574e5eeccdfcd
Arturo <md.arturo@gmail.com>
2025-10-22 18:48:59 +0200

    Crear la función get_owned_currencies

--- inversion_app/models/transactions.py
+++ inversion_app/models/transactions.py
@@ -21,3 +21,26 @@ def insert_transaction(data_form):
 
     except sqlite3.Error as e:
         return f"No se ha podido guardar la transacción: {e}"
+    
+def get_owned_currencies():
+    """
+    Returns a dictionary with all coins with a positive balance in database as key and it's balance as value.
+    """
+    try:
+        query = "SELECT currency_from, amount_from, currency_to, amount_to FROM transactions"
+        conn = Connection(query)
+        rows = conn.response.fetchall()
+        conn.connection.close()
+
+        balances = {}
+
+        for row in rows:
+            balances[row["currency_from"]] = balances.get(row["currency_from"], 0) - row["amount_from"]
+            balances[row["currency_to"]] = balances.get(row["currency_to"], 0) + row["amount_to"]
+
+        owned_currencies = {currency: balance for currency, balance in balances.items() if balance > 0}
+
+        return owned_currencies
+
+    except sqlite3.Error as e:
+        return f"No se ha podido obtener el balance: {e}"
