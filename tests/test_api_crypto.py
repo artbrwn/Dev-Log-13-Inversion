00db2d0a446604c0c45725f3aa3c755f7d51a666
Arturo <md.arturo@gmail.com>
2025-11-09 16:20:20 +0100

    Añadir tests básicos para la clase ApiCrypto

---
+++ tests/test_api_crypto.py
@@ -0,0 +1,47 @@
+import inversion_app.models.api_crypto as api_cripto
+import pytest
+import unittest.mock as mock
+from inversion_app.models.api_crypto import ApiCrypto
+from requests import RequestException
+
+
+@mock.patch("inversion_app.models.api_crypto.requests.get")
+def test_get_good_conversion_price(mock_get):
+    data = {"status": {
+                        "timestamp": "2025-10-24T10:27:48.051Z",
+                        "error_code": 0,
+                        "error_message": None,
+                        "elapsed": 12,
+                        "credit_count": 1,
+                        "notice": None
+                    },
+                    "data": {
+                        "id": 1,
+                        "symbol": "BTC",
+                        "name": "Bitcoin",
+                        "amount": 1,
+                        "last_updated": "2025-10-24T10:25:00.000Z",
+                        "quote": {
+                            "2790": {
+                                "price": 95680.67636668718,
+                                "last_updated": "2025-10-24T10:26:02.000Z"
+                            }
+                        }
+                    }
+                }
+    mock_response = mock.Mock()
+    mock_response.json.return_value = data
+    mock_get.return_value = mock_response
+    api_control = api_cripto.ApiCrypto()
+    conversion_price = api_control.get_conversion_price(1, 1, 2790)
+
+    assert conversion_price == data["data"]["quote"]["2790"]["price"]
+
+@mock.patch("inversion_app.models.api_crypto.requests.get")
+def test_capture_exception(mock_get):
+    mock_get.side_effect = RequestException("API error")
+    api_control = ApiCrypto()
+    with pytest.raises(api_cripto.ApiCryptoError) as e:
+        api_control.get_conversion_price(1, 1, 2790)
+
+    assert "API request/conversion failed" in str(e)
